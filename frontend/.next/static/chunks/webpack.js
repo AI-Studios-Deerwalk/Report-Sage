e8f7a22a13a22c2ee8124b882a1626b602dfed60
ason-opps <ason.gautam@deerwalk.edu.np>
2025-08-16 20:52:41 +0545

    omg

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f564e39c5672efa2"; }
+/******/ 		__webpack_require__.h = function() { return "34ed573b8a715b58"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
