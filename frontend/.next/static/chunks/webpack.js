5021ed7484fd502ffdb7e9b13838e16c93a1bc9d
samyamcodes2028 <samyam.humagain@deerwalk.edu.np>
2025-08-29 15:02:32 +0545

    fix:prompt changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f7729b58a42f46f"; }
+/******/ 		__webpack_require__.h = function() { return "a0c3fd1fb353a49c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
